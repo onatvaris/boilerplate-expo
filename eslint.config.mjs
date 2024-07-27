d969822499094327517d98498b504948a7aff65c
onatvaris <onatvaris@gmail.com>
2024-07-28 02:33:56 +0300

    chore(eslint): :hammer: update eslint and typescript-eslint versions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,8 +5,5 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    rules: {}
-  }
+  ...tseslint.configs.recommended
 );
