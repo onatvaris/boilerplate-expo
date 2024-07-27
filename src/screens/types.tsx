147365924281999262ea5dfaae254a70925dbe2b
onatvaris <onatvaris@gmail.com>
2024-07-28 01:53:06 +0300

    chore: :hammer: update LoginScreen component to remove unused prop

--- src/screens/types.tsx
+++ src/screens/types.tsx
@@ -27,6 +27,7 @@ export type HomeDrawerScreenProps<
 >;
 
 declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace ReactNavigation {
     interface RootParamList extends RootStackParamList {}
   }
